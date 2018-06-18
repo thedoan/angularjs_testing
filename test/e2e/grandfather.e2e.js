f457ca4b0a5a2480d81e7780c9745df304b247af
The Doan <dangthedoan@gmail.com>
2018-06-19 01:18:53 +0700

    Fix grandfather.e2e.js to remove duplicate
    Remove duplicate variable question, answer, button by moving them out of
    spec to the top

--- test/e2e/grandfather.e2e.js
+++ test/e2e/grandfather.e2e.js
@@ -1,24 +1,19 @@
 describe("The grandfather of all knowledge", function() {
+	var question = element(by.model('question.text'));
+	var answer = element(by.binding('answer'));
+	var button = element(by.className('question__button'));
 
 	beforeEach(function() {
 		browser.get('http://localhost:8000/grandfather.html');
 	});
 
 	it('should answer any question', function() {
-		var question = element(by.model('question.text'));
-		var answer = element(by.binding('answer'));
-		var button = element(by.className('question__button'));
-
 		question.sendKeys("What is the purpose of meaning?");
 		button.click();
 		expect(answer.getText()).toEqual("Chocolate!");
 	});
 
 	it('should not allow empty questions', function() {
-		var question = element(by.model('question.text'));
-		var answer = element(by.binding('answer'));
-		var button = element(by.className('question__button'));
-
 		question.sendKeys("    ");
 		expect(button.isEnabled()).toBeFalsy();
 	});
