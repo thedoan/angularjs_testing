1df4b29160de93b4d0a6405df3e920ec7118a918
The Doan <dangthedoan@gmail.com>
2018-06-19 15:17:08 +0700

    Fix unit test chapter04.spec.js
    Fix chapter04.spec.js to remove duplicate

--- test/unit/chapter04.spec.js
+++ test/unit/chapter04.spec.js
@@ -1,68 +1,38 @@
 'use strict';
 
 describe('Testing Controllers', function () {
-	var rootScope;
-	var scope;
+	var $rootScope;
+	var $scope;
+	var $controller;
 
-	beforeEach(module('chapter4'));
+	beforeEach(module('chapter04'));
 	//setup for testing controller
-	beforeEach(inject(function ($rootScope) {
-		rootScope = $rootScope;
-		scope = $rootScope.$new();
+	beforeEach(inject(function (_$rootScope_,_$controller_) {
+		$rootScope = _$rootScope_;
+		$scope = $rootScope.$new();
+		$controller = _$controller_;
+		$controller('HomeCtrl', {
+			$scope: $scope
+		});
 	}));
 
 	describe('Getting started testing a controller', function () {
-
-		beforeEach(inject(function ($controller) {
-			$controller('HomeCtrl', {
-				$scope: scope
-			});
-		}));
-
 		it('should do something', function () {
-			expect(scope.id).toBe('foo');
+			expect($scope.id).toBe('foo');
 		});
 
 	});
 
 	describe('Testing initial state of a scope object', function () {
-
-		beforeEach(inject(function ($controller) {
-			$controller('HomeCtrl', {
-				$scope: scope
-			});
-		}));
-
 		it('should set the scope property id to the correct initial value', function () {
-			expect(scope.id).toBe('foo');
+			expect($scope.id).toBe('foo');
 		});
 
 	});
 
 	describe('Testing interactive scope changes using Protractor', function () {
-
-		beforeEach(inject(function ($controller) {
-			$controller('HomeCtrl', {
-				$scope: scope
-			});
-		}));
-
 		it('should do something', function () {
-			expect(scope.id).toBe('foo');
-		});
-
-	});
-
-	describe('Testing navigation scope changes using Protractor', function () {
-
-		beforeEach(inject(function ($controller) {
-			$controller('HomeCtrl', {
-				$scope: scope
-			});
-		}));
-
-		it('should do something', function () {
-			expect(scope.id).toBe('foo');
+			expect($scope.id).toBe('foo');
 		});
 
 	});
