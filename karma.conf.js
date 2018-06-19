f9f7536133dc447f45a24628937ed827e19b2591
The Doan <dangthedoan@gmail.com>
2018-06-19 17:50:32 +0700

    Add chapter05
    Testing user interaction and directives

--- karma.conf.js
+++ karma.conf.js
@@ -4,12 +4,16 @@
 module.exports = function() {
 	return {
 		frameworks: ['jasmine'],
+		preprocessors: {
+			'*.html': ['ng-html2js']
+		},
 		files: [
 			"bower_components/angular/angular.min.js",
 			"bower_components/angular-route/angular-route.min.js",
 			"bower_components/angular-ui-router/release/angular-ui-router.min.js",
 			"bower_components/angular-mocks/angular-mocks.js",
 			"src/*.js",
+			"*.html",
 			"test/unit/*.spec.js"
 		],
 		autoWatch: true,
